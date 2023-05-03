cef345e6c70a020ec4188e2eed31fd8c37b38703
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-05-03 17:13:02 +0300

    Update chimera_mix.py

--- chimera_mix.py
+++ chimera_mix.py
@@ -4,13 +4,13 @@ import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
 
-def generate_chimeras(input_files, output_file, chimera_info_file, num_chimeras, chimera_id_prefix="chimera"):
+def generate_chimeras(selected_input_file, input_files, output_file, chimera_info_file, num_chimeras, chimera_id_prefix="chimera"):
     records = []
     for input_file in input_files:
         records.extend(list(SeqIO.parse(input_file, "fasta")))
 
-    main_records = records[:len(records)//10]  # 10% of records from the first file
-    mixed_records = records  # 90% of chimeric reads from a mix of sequences
+    main_records = list(SeqIO.parse(selected_input_file, "fasta"))
+    mixed_records = records
 
     chimeras = []
 
@@ -22,7 +22,7 @@ def generate_chimeras(input_files, output_file, chimera_info_file, num_chimeras,
             breakpoint = random.randint(1, len(seq1) - 1)
             chimera_seq = seq1[:breakpoint] + seq2[breakpoint:]
 
-            if i < int(num_chimeras * 0.04):  # 4% of 10% portion with reversed sequences
+            if i < int(num_chimeras * 0.04):
                 chimera_seq = chimera_seq.reverse_complement()
 
             chimera_id = f"{chimera_id_prefix}_{i}"
@@ -33,19 +33,29 @@ def generate_chimeras(input_files, output_file, chimera_info_file, num_chimeras,
             reversed_status = "yes" if i < int(num_chimeras * 0.04) else "no"
             chimera_info_handle.write(f"{chimera_id}\t{seq1.id}\t{seq2.id}\t{breakpoint}\t{reversed_status}\n")
 
+    # Insert chimeric reads at specified positions
+    for chimera in chimeras:
+        position = random.randint(0, len(main_records) - 1)
+        main_records.insert(position, chimera)
+
     with open(output_file, "w") as output_handle:
-        SeqIO.write(main_records + chimeras, output_handle, "fasta")
+        SeqIO.write(main_records, output_handle, "fasta")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print(f"Usage: {sys.argv[0]} input_directory output_fasta chimera_info_file num_chimeras")
+    if len(sys.argv) != 6:
+        print(f"Usage: {sys.argv[0]} selected_input_file input_directory output_fasta chimera_info_file num_chimeras")
         sys.exit(1)
 
-    input_directory = sys.argv[1]
-    output_fasta = sys.argv[2]
-    chimera_info_file = sys.argv[3]
-    num_chimeras = int(sys.argv[4])
+    selected_input_file = sys.argv[1]
+    input_directory = sys.argv[2]
+    output_fasta = sys.argv[3]
+    chimera_info_file = sys.argv[4]
+    num_chimeras = int(sys.argv[5])
 
     input_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".fasta")]
 
-    generate_chimeras(input_files, output_fasta, chimera_info_file, num_chimeras)
+    generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file, num_chimeras)
+
+
+#run the code with this command: 
+python chimera_mix.py homo.fasta . new.fasta chimera_info.tsv 100
