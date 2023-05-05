0d7f0b42ac4355dd642b18d57cdccd2314937a5c
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-05-05 16:47:27 +0300

    Update chimera.py
    comments added

--- chimera.py
+++ chimera.py
@@ -6,21 +6,25 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
+# Function to mutate a sequence based on the mutation_rate
 def mutate_sequence(seq_record, mutation_rate):
     sequence = str(seq_record.seq)
     mutated_sequence = []
 
+    # Iterate through each base and mutate with the specified mutation_rate
     for base in sequence:
         if random.random() < mutation_rate:
             mutated_sequence.append(random.choice('ACTG'.replace(base, '')))
         else:
             mutated_sequence.append(base)
 
+    # Create a new SeqRecord with the mutated sequence
     mutated_seq_string = "".join(mutated_sequence)
     mutated_seq_record = SeqRecord(Seq(mutated_seq_string, IUPAC.ambiguous_dna), id=seq_record.id, description=seq_record.description)
 
     return mutated_seq_record
 
+# Function to generate chimeric reads
 def generate_chimeras(selected_input_file, input_files, output_file, chimera_info_file, chimera_id_prefix="chimera"):
     records = []
     for input_file in input_files:
@@ -29,6 +33,7 @@ def generate_chimeras(selected_input_file, input_files, output_file, chimera_inf
     main_records = list(SeqIO.parse(selected_input_file, "fasta"))
     mixed_records = records
 
+    # Calculate the number of chimeras based on the input file's total reads
     total_reads = len(main_records)
     num_chimeras = int(total_reads * random.uniform(0.01, 0.03))  # 1-3% chimeric reads
 
@@ -62,19 +67,22 @@ def generate_chimeras(selected_input_file, input_files, output_file, chimera_inf
         position = random.randint(0, len(main_records) - 1)
         main_records.insert(position, chimera)
 
+    # Write the output file with chimeric reads
     with open(output_file, "w") as output_handle:
         SeqIO.write(main_records, output_handle, "fasta")
 
+# Main function to execute the script
 if __name__ == "__main__":
     if len(sys.argv) != 6:
         print(f"Usage: {sys.argv[0]} selected_input_file input_directory output_fasta chimera_info_file")
         sys.exit(1)
-
     selected_input_file = sys.argv[1]
     input_directory = sys.argv[2]
     output_fasta = sys.argv[3]
     chimera_info_file = sys.argv[4]
 
-    input_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".fasta")]
-    
-    generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file)
+# Get input_files in the input_directory with a .fasta extension
+input_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".fasta")]
+
+# Generate chimeric reads and write them to the output file
+generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file)
