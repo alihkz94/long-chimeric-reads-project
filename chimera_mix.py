03c1fd6222160f7849aebb30e320edc51d5ae1a2
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-05-08 10:04:37 +0300

    Update chimera_mix.py

--- chimera_mix.py
+++ chimera_mix.py
@@ -3,8 +3,38 @@ import random
 import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
-def generate_chimeras(selected_input_file, input_files, output_file, chimera_info_file, num_chimeras, chimera_id_prefix="chimera"):
+# Function to mutate a sequence based on the mutation_rate
+def mutate_sequence(seq_record, mutation_rate):
+    sequence = str(seq_record.seq)
+    mutated_sequence = []
+
+    # Iterate through each base and mutate with the specified mutation_rate
+    for base in sequence:
+        if random.random() < mutation_rate:
+            mutated_sequence.append(random.choice('ACTG'.replace(base, '')))
+        else:
+            mutated_sequence.append(base)
+
+    # Create a new SeqRecord with the mutated sequence
+    mutated_seq_string = "".join(mutated_sequence)
+    mutated_seq_record = SeqRecord(Seq(mutated_seq_string), id=seq_record.id, description=seq_record.description)
+
+    return mutated_seq_record
+
+#chimeric sequences generation
+def calculate_abundance_ratio(records):
+    seq_abundance = {}
+    for record in records:
+        seq_abundance[record.id] = seq_abundance.get(record.id, 0) + 1
+
+    total_seqs = len(records)
+    seq_ratios = {seq_id: count / total_seqs for seq_id, count in seq_abundance.items()}
+    return seq_ratios
+
+
+def generate_chimeras(selected_input_file, input_files, output_file, chimera_info_file, chimera_id_prefix="chimera"):
     records = []
     for input_file in input_files:
         records.extend(list(SeqIO.parse(input_file, "fasta")))
@@ -12,50 +42,83 @@ def generate_chimeras(selected_input_file, input_files, output_file, chimera_inf
     main_records = list(SeqIO.parse(selected_input_file, "fasta"))
     mixed_records = records
 
+    original_ratios = calculate_abundance_ratio(main_records)
+
+    # Calculate the number of chimeras based on the input file's total reads
+    total_reads = len(main_records)
+    num_chimeras = int(total_reads * random.uniform(0.01, 0.03))  # 1-3% chimeric reads
+
     chimeras = []
 
     with open(chimera_info_file, "w") as chimera_info_handle:
-        chimera_info_handle.write("chimera_id\tseq1_id\tseq2_id\tbreakpoint\treversed\n")
+        chimera_info_handle.write("chimera_id\tseq1_id\tseq2_id\tbreakpoint\treversed\tratio\n")
 
         for i in range(num_chimeras):
-            seq1, seq2 = random.sample(mixed_records, 2)
+            if i < int(num_chimeras * 0.1):
+                seq1, seq2 = random.sample(main_records, 2)
+            else:
+                seq1, seq2 = random.sample(mixed_records, 2)
+
             breakpoint = random.randint(1, len(seq1) - 1)
-            chimera_seq = seq1[:breakpoint] + seq2[breakpoint:]
+            chimera_seq = seq1.seq[:breakpoint] + seq2.seq[breakpoint:]
 
             if i < int(num_chimeras * 0.04):
                 chimera_seq = chimera_seq.reverse_complement()
 
             chimera_id = f"{chimera_id_prefix}_{i}"
-            chimera_seq.id = chimera_id
-            chimera_seq.description = ""
-            chimeras.append(chimera_seq)
+            chimera_record = SeqRecord(Seq(str(chimera_seq)), id=chimera_id, description="")
+
+            # Introduce mutations
+            mutation_rate = 0.005  # Adjust this value based on the desired mutation rate
+            chimera_record = mutate_sequence(chimera_record, mutation_rate)
+
+            chimeras.append(chimera_record)
 
             reversed_status = "yes" if i < int(num_chimeras * 0.04) else "no"
-            chimera_info_handle.write(f"{chimera_id}\t{seq1.id}\t{seq2.id}\t{breakpoint}\t{reversed_status}\n")
+            original_ratio = original_ratios[seq1.id]
+            ratio = original_ratio * random.uniform(0.9, 0.985)  # 1.5% to 10% lower ratio
+            chimera_info_handle.write(f"{chimera_id}\t{seq1.id}\t{seq2.id}\t{breakpoint}\t{reversed_status}\t{ratio}\n")
 
     # Insert chimeric reads at specified positions
     for chimera in chimeras:
         position = random.randint(0, len(main_records) - 1)
         main_records.insert(position, chimera)
 
-    with open(output_file, "w") as output_handle:
+    # Write the output file with chimeric reads
+    with open(output_file,"w") as output_handle:
         SeqIO.write(main_records, output_handle, "fasta")
 
+# Main function to execute the script
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        print(f"Usage: {sys.argv[0]} selected_input_file input_directory output_fasta chimera_info_file num_chimeras")
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} selected_input_file input_directory output_fasta chimera_info_file")
         sys.exit(1)
-
     selected_input_file = sys.argv[1]
     input_directory = sys.argv[2]
     output_fasta = sys.argv[3]
     chimera_info_file = sys.argv[4]
-    num_chimeras = int(sys.argv[5])
 
+    # Get input_files in the input_directory with a .fasta extension
     input_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".fasta")]
 
-    generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file, num_chimeras)
+    # Generate chimeric reads and write them to the output file
+    generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file)
 
 
-#run the code with this command: 
-python chimera_mix.py homo.fasta . new.fasta chimera_info.tsv 100
+
+# Main function to execute the script
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} selected_input_file input_directory output_fasta chimera_info_file")
+        sys.exit(1)
+    selected_input_file = sys.argv[1]
+    input_directory = sys.argv[2]
+    output_fasta = sys.argv[3]
+    chimera_info_file = sys.argv[4]
+
+    # Get input_files in the input_directory with a .fasta extension
+    input_files = [os.path.join(input_directory, file) for file in os.listdir(input_directory) if file.endswith(".fasta")]
+
+    # Generate chimeric reads and write them to the output file
+    generate_chimeras(selected_input_file, input_files, output_fasta, chimera_info_file)
+
