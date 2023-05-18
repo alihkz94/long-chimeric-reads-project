d4fd2f7a3e39a1064eebeaf22676ab9bfa1d8f61
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-05-18 13:27:02 +0300

    Update cutadapt.sh

--- cutadapt.sh
+++ cutadapt.sh
@@ -2,13 +2,13 @@
 
 #Cutadapt_ITS : 
 
-#cutadapt -a KCNGTWGGWGAACCWGC \
-#-g "CATATHANTAAGSSSAGG;min_overlap=16" \
-#-e 2 -j 8 \
-#--discard-untrimmed \
-#--action=retain \
-#-o ITS.fasta \
-#fungal_species.fasta > result_ITS.txt
+cutadapt -a KCNGTWGGWGAACCWGC \
+-g "CATATHANTAAGSSSAGG;min_overlap=16" \
+-e 2 -j 8 \
+--discard-untrimmed \
+--action=retain \
+-o ITS.fasta \
+fungal_species.fasta > result_ITS.txt
 
 
 #Cutadapt_SSU: 
