2c9975bb63c02b82b6716dc4b17d26a21c0990db
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-06-13 15:34:27 +0300

    Update convert_fasta.sh

--- convert_fasta.sh
+++ convert_fasta.sh
@@ -3,8 +3,20 @@
 # Create FASTA folder
 mkdir -p FASTA
 
+# Log file for error messages
+error_log="error.log"
+
 # Find all fastq files and convert them to FASTA using seqkit
 for fastq_file in *.fastq; do
     fasta_file="FASTA/${fastq_file%.fastq}.fasta"
-    seqkit fq2fa "$fastq_file" -o "$fasta_file"
+    
+    # Run seqkit fq2fa and redirect stderr to the error log file
+    seqkit fq2fa "$fastq_file" -o "$fasta_file" 2>> "$error_log"
+    
+    # Check if the FASTA file was generated
+    if [ -f "$fasta_file" ]; then
+        echo "Conversion successful: $fastq_file -> $fasta_file"
+    else
+        echo "Conversion failed: $fastq_file" >> "$error_log"
+    fi
 done
