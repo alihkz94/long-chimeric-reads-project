acc9056b307e2edca13112e68fe08323906f6abd
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-06-16 14:06:26 +0300

    Update uchime.py

--- uchime.py
+++ uchime.py
@@ -1,5 +1,6 @@
 import subprocess
 import matplotlib.pyplot as plt
+from Bio import SeqIO
 
 # Define the parameters
 parameters = {
@@ -16,6 +17,33 @@ output_file = 'output.fasta'
 
 # Define the base command
 base_cmd = f'vsearch --uchime_denovo {input_file} --abskew 1.5 --nonchimeras {output_file}'
+
+# Function to parse the chimera_info_file and store it in a dictionary
+def parse_chimera_info(chimera_info_file):
+    chimera_dict = {}
+    with open(chimera_info_file, "r") as f:
+        next(f)  # skip header
+        for line in f:
+            chimera_id, seq1_id, seq2_id, breakpoint = line.strip().split("\t")
+            chimera_dict[chimera_id] = (seq1_id, seq2_id, int(breakpoint))
+    return chimera_dict
+
+# Function to parse the FASTA file of detected chimeric sequences and store the IDs in a set
+def parse_detected_chimeras(fasta_file):
+    detected_chimeras = set()
+    with open(fasta_file, "r") as f:
+        for record in SeqIO.parse(f, "fasta"):
+            detected_chimeras.add(record.id.split(";")[0])  # Remove ";size=2" from the ID
+    return detected_chimeras
+
+# Function to calculate true positives, false positives, true negatives, and false negatives
+def calculate_rates(chimera_dict, detected_chimeras, total_non_chimera_reads):
+    true_positives = len(detected_chimeras.intersection(chimera_dict.keys()))
+    false_positives = len(detected_chimeras.difference(chimera_dict.keys()))
+    false_negatives = len(chimera_dict.keys()) - true_positives
+    true_negatives = total_non_chimera_reads - false_positives
+
+    return true_positives, false_positives, true_negatives, false_negatives
 
 # Loop over the parameters
 results = {}
@@ -34,6 +62,19 @@ for param, values in parameters.items():
         
         # Store the result
         results[param].append(num_chimeras)
+
+        # Parse the chimera_info_file and detected_chimeras_fasta
+        chimera_dict = parse_chimera_info('chimera_info.txt')
+       detected_chimeras = parse_detected_chimeras(output_file)
+
+        # Calculate rates
+        tp, fp, tn, fn = calculate_rates(chimera_dict, detected_chimeras, total_non_chimera_reads)
+
+        print(f"For {param} = {value}:")
+        print(f"True Positives: {tp}")
+        print(f"False Positives: {fp}")
+        print(f"True Negatives: {tn}")
+        print(f"False Negatives: {fn}")
 
 # Plot the results
 for param, values in parameters.items():
