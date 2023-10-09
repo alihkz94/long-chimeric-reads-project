2422cae0b14d5f1d2ff9208d6a5bb2200a3b46b9
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-10-09 08:31:52 +0300

    Update treat_chimeras.sh
    updated for HPC

--- treat_chimeras.sh
+++ treat_chimeras.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name="blast_treat"
+#SBATCH --cpus-per-task=128
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+#SBATCH --partition amd
+#SBATCH --time=10:00:00
+#SBATCH --output=blast_treat.out
+
 set -e # Exit on error
 
 # Input: FASTA file for chimera filtering
-INPUT_FILE="input_sequences.fasta"
-REFERENCE_DB="path_to_reference_db.fasta"  # Modify this to the path of your reference database for BLAST
+INPUT_FILE="/gpfs/space/home/alihakim/analysis/treat_chimera/metabar_uchime_input.fasta"
+REFERENCE_DB="/gpfs/space/home/alihakim/analysis/treat_chimera/ITS.fasta"  # Modify this to the path of your reference database for BLAST
 
 # 1. Pre-dereplication
 echo "Pre-dereplication..."
@@ -15,12 +23,12 @@ vsearch --cluster_size dereplicated.fasta --id 0.97 --sizein --sizeout --fasta_w
 
 # 3. Chimera Filtering with UCHIME Denovo
 echo "Chimera Filtering with UCHIME Denovo..."
-vsearch --uchime_denovo preclustered.fasta --mindiv 0.5 --dn 1.6 --threads 8 --uchimeout denovo.uchime \
+vsearch --uchime_denovo preclustered.fasta --mindiv 0.4 --dn 1.6 --minh 0.01 --threads 128 --uchimeout denovo.uchime \
 --nonchimeras denovo_nonchimeras.fasta --chimeras denovo_chimeras.fasta
 
 # 4. Chimera Filtering with UCHIME Ref
 echo "Chimera Filtering with UCHIME Ref..."
-vsearch --uchime_ref denovo_nonchimeras.fasta --mindiv 0.5 --dn 1.6 --threads 8 \
+vsearch --uchime_ref denovo_nonchimeras.fasta --mindiv 0.4 --dn 1.6 --minh 0.01 --threads 128 \
 --db $REFERENCE_DB \
 --nonchimeras ref_nonchimeras.fasta --chimeras ref_chimeras.fasta
 
@@ -31,10 +39,9 @@ echo "BLAST analysis for flagged chimeric sequences..."
 cat denovo_chimeras.fasta ref_chimeras.fasta > combined_chimeras.fasta
 
 blastn -query combined_chimeras.fasta \
--db $REFERENCE_DB \
--task blastn \
+-db /gpfs/space/home/alihakim/analysis/databse/UNITE  \
 -word_size 7 \
--num_threads 12 \
+-num_threads 128 \
 -outfmt "6 delim=+ qseqid stitle qlen slen qstart qend sstart send evalue length nident mismatch gapopen gaps sstrand qcovs pident" \
 -evalue 0.001 \
 -strand both \
