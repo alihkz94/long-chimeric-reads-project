c53c64896e952da986a93cbe4497083a0454c89a
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-10-17 14:42:49 +0300

    Update dada2_chimera.R
    Changed the default settings and updated to True ones

--- DADA2/dada2_chimera.R
+++ DADA2/dada2_chimera.R
@@ -5,8 +5,8 @@ library(Rcpp)
 library(dada2)
 library(ShortRead)
 library(tibble)
-library(Biostrings)
-setwd("~/Documents/simulated_data/analysis/DADA2")
+setwd("/gpfs/space/home/alihakim/analysis/dada2")
+
 # Function to read fasta sequences into a named vector
 read_fasta_to_vector <- function(fasta_file) {
   fasta_seqs <- readDNAStringSet(fasta_file)
@@ -28,15 +28,10 @@ ground_truth <- ifelse(seqtab$Sequence %in% known_chimeras, 1, 0)
 
 # List of options to test, tailored for PacBio
 options_list <- list(
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 100),
-  list(minFoldParentOverAbundance = 2.5, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 100),
-  list(minFoldParentOverAbundance = 3, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 100),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 15, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 100),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 20, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 100),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 150),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 6, maxShift = 200),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 7, maxShift = 100),
-  list(minFoldParentOverAbundance = 2, minParentAbundance = 10, allowOneOff = TRUE, minOneOffParentDistance = 8, maxShift = 100)
+    list(minSampleFraction = 0.9,ignoreNNegatives=1,minFoldParentOverAbundance = 1.5, 
+    minParentAbundance = 2, allowOneOff = FALSE, minOneOffParentDistance = 2, maxShift =16),
+      list(minSampleFraction = 0.9,ignoreNNegatives=1,minFoldParentOverAbundance = 1.5,
+      minParentAbundance = 2, allowOneOff = TRUE, minOneOffParentDistance = 2, maxShift =16)
 )
 
 # Initialize a data frame to store metrics
@@ -44,8 +39,10 @@ metrics_df <- tibble()
 
 # Loop through options and perform chimera removal
 for (opts in options_list) {
-  cleaned_seqtab_matrix <- removeBimeraDenovo(t(seqtab_matrix),
+  cleaned_seqtab_matrix <- removeBimeraDenovo(t(seqtab_matrix), method = "consensus",
                                               minFoldParentOverAbundance = opts$minFoldParentOverAbundance,
+                                              minSampleFraction = opts$minSampleFraction,
+                                              ignoreNNegatives = opts$ignoreNNegatives,
                                               minParentAbundance = opts$minParentAbundance,
                                               allowOneOff = opts$allowOneOff,
                                               minOneOffParentDistance = opts$minOneOffParentDistance,
