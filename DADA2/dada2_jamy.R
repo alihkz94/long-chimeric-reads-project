e5ac3dec45bb75d24ae6ab64fe50fb197d16e378
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2024-02-15 21:18:05 +0200

    Update dada2_jamy.R

--- DADA2/dada2_jamy.R
+++ DADA2/dada2_jamy.R
@@ -65,3 +65,75 @@ fasta_content <- unlist(fasta_lines)
 
 # Write the FASTA file
 writeLines(fasta_content, con = "chimeric_sequences.fasta")
+
+
+
+################Cluster script#############
+# Libraries
+library(BiocGenerics)
+library(Biostrings)
+library(Rcpp)
+library(dada2)
+library(ShortRead)
+library(tibble)
+library(seqinr)
+library(Hmisc)
+library(openssl)
+
+# Set working directory
+setwd("/gpfs/space/home/alihakim/mypc/dada2/66")
+
+# Load pre-processed FASTA files (assumed dereplicated)
+fasta_files <- list.files(pattern="*.fasta", full.names=TRUE)
+
+# Initialize an empty list for sequence tables
+seqtabs <- list()
+
+for(fasta_file in fasta_files) {
+  seqs <- readDNAStringSet(fasta_file)
+  seqtabs[[basename(fasta_file)]] <- table(sapply(seqs, as.character))
+}
+
+# Convert the list of tables into a single data frame
+seqtab <- do.call(rbind, seqtabs)
+
+# Ensure column names are unique sequences
+colnames(seqtab) <- make.names(colnames(seqtab), unique=TRUE)
+
+# Optionally, save sequence table for later use
+saveRDS(seqtab, "sequence_table.rds")
+# Load sequence table if needed
+seqtab <- readRDS("sequence_table.rds")
+
+# Chimera removal (if not already done)
+nonchimeric_seqtab <- removeBimeraDenovo(seqtab, method = "consensus", multithread = TRUE,
+                                         minFoldParentOverAbundance = 1.5, minParentAbundance = 2, 
+                                         allowOneOff = TRUE, minOneOffParentDistance = 2, maxShift = 16, 
+                                         verbose = TRUE)
+
+# Number of sequences before and after
+orig_count <- ncol(seqtab)  
+filtered_count <- ncol(nonchimeric_seqtab)
+discarded_count <- orig_count - filtered_count
+
+# Extract chimeric sequences
+discarded_chimera <- seqtab[,colnames(seqtab) %nin% colnames(nonchimeric_seqtab)]
+chimeric_sequences <- getSequences(discarded_chimera)
+
+# Create a DNAStringSet object
+chimeric_dna <- DNAStringSet(chimeric_sequences)
+
+# Ensure that chimeric_sequences are character strings
+chimeric_sequences <- as.character(chimeric_dna)
+
+# Create headers for the FASTA file
+chimera_headers <- paste0(">", "chimera_", seq_along(chimeric_sequences))
+
+# Combine headers and sequences into a FASTA format
+fasta_lines <- Map(function(header, sequence) paste(header, sequence, sep = "\n"), chimera_headers, chimeric_sequences)
+
+# Flatten the list into a character vector
+fasta_content <- unlist(fasta_lines)
+
+# Write the FASTA file
+writeLines(fasta_content, con = "chimeric_sequences.fasta")
