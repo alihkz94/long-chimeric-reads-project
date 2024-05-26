6cfb07efe9fa3027035c37febd5700d8819a7c76
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2024-05-26 09:00:04 +0000

    update

--- UCHIME/uchime_denovo.py
+++ UCHIME/uchime_denovo.py
@@ -19,43 +19,38 @@ parameters = {
 input_file = 'metabar_uchime_input.fasta'
 
 def parse_chimera_info(filename):
-    # Define the column names
     column_names = ["chimera_id", "seq1_id", "seq1_file", "seq2_id", "seq2_file", "breakpoint", "reversed_status", "ratio", "chimera_seq_length"]
-
-    # Load the data
     data = pd.read_csv(filename, sep="\t", header=None, names=column_names)
-
-    # Convert the DataFrame to a dictionary
-    data_dict = data.to_dict(orient="records")
-
-    return data_dict
+    return data.to_dict(orient="records")
 
 # Dereplicate sequences
-dereplicate_cmd = f'vsearch --derep_fulllength {input_file} --sizein --sizeout --fasta_width 0 --output tempdir/{input_file}.derep.fasta'
-
-# Run the dereplication command
+dereplicate_cmd = f'vsearch --derep_fulllength {input_file} --sizein --sizeout --fasta_width 0 --output {tempdir}/{input_file}.derep.fasta'
 subprocess.run(dereplicate_cmd, shell=True)
 
-# Loop over the parameters
+# Loop over the parameters for chimera filtering
 results = {}
 for param, values in parameters.items():
     results[param] = []
     for value in values:
-        # Construct the uchime command
         output_file = f'output_{param}_{value}.fasta'
-        uchime_cmd = f'vsearch --uchime_denovo tempdir/{input_file}.derep.fasta {param} {value} --sizein --sizeout --fasta_width 0 \
-            --chimeras {output_file}'
+        uchime_cmd = f'vsearch --uchime_denovo {tempdir}/{input_file}.derep.fasta {param} {value} --sizein --sizeout --fasta_width 0 \
+            --chimeras {tempdir}/{output_file} --uchimeout {tempdir}/uchime_out_{param}_{value}.tsv --borderline {tempdir}/borderline_{param}_{value}.fasta'
 
-        # Run the command
         subprocess.run(uchime_cmd, shell=True)
 
-        # Parse the output to get the number of chimeras
-        chimera_info_file = os.path.join('output_directory', f"chimera_info_{param}_{value}.tsv")
+        # Combine chimeric and borderline reads
+        combined_file = f'{tempdir}/combined_{param}_{value}.fasta'
+        with open(combined_file, 'w') as outfile:
+            for fname in [f'{tempdir}/{output_file}', f'{tempdir}/borderline_{param}_{value}.fasta']:
+                with open(fname) as infile:
+                    outfile.write(infile.read())
+
+        # Parse UCHIME output
+        chimera_info_file = f'{tempdir}/uchime_out_{param}_{value}.tsv'
         if os.path.exists(chimera_info_file):
             chimera_dict = parse_chimera_info(chimera_info_file)
             num_chimeras = len(chimera_dict)
         else:
-            num_chimeras = 0  # If the file does not exist, assume no chimeras were found
+            num_chimeras = 0
 
-        # Store the result
         results[param].append(num_chimeras)
