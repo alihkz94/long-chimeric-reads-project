c030fad0838195b6144792fadc1cfe2a3f0f05a5
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2024-06-19 17:37:01 +0300

    Update uchime_denovo.sh

--- real data/uchime_denovo.sh
+++ real data/uchime_denovo.sh
@@ -1,3 +1,5 @@
+###### WITH DEREPLICATION ########
+
 #!/bin/bash
 
 # Directory containing your FASTA files
@@ -17,7 +19,36 @@ for FASTA_FILE in "$FASTA_DIR"/*.fasta; do
 
     # 2. Chimera Filtering with UCHIME Denovo
     echo "Chimera Filtering with UCHIME Denovo for $BASENAME..."
-    vsearch --uchime_denovo "dereplicated/${BASENAME}_dereplicated.fasta" --mindiv 0.4 --dn 1.6 --minh 0.08 \
+    vsearch --uchime_denovo "dereplicated/${BASENAME}_dereplicated.fasta" --mindiv 0.4 --dn 1.6 --minh 0.08 --sizein --sizeout --fasta_width 0 \
+    --nonchimeras "chimeras/${BASENAME}_denovo_nonchimeras.fasta" --chimeras "chimeras/${BASENAME}_denovo_chimeras.fasta" --borderline "chimeras/${BASENAME}_denovo_borderline.fasta"
+
+    # Combining chimeric and borderline sequences for each file
+    echo "Combining chimeric and borderline sequences for $BASENAME..."
+    cat "chimeras/${BASENAME}_denovo_chimeras.fasta" "chimeras/${BASENAME}_denovo_borderline.fasta" > "chimeras/combined_chimeras/${BASENAME}_combined_chimeras.fasta"
+done
+
+echo "Script completed."
+
+
+
+###### WITHOUT DEREPLICATION ########
+
+#!/bin/bash
+
+# Directory containing your FASTA files
+FASTA_DIR="/home/ali/Documents/simulated_data/analysis/tagJump_filt/original"
+
+# Create folders for each step
+mkdir -p chimeras
+mkdir -p chimeras/combined_chimeras  # Create subfolder for combined chimeras
+
+# Loop through each FASTA file in the directory
+for FASTA_FILE in "$FASTA_DIR"/*.fasta; do
+    BASENAME=$(basename "$FASTA_FILE" .fasta)
+
+    # Chimera Filtering with UCHIME Denovo
+    echo "Chimera Filtering with UCHIME Denovo for $BASENAME..."
+    vsearch --uchime_denovo "$FASTA_FILE" --mindiv 0.4 --dn 1.6 --minh 0.08 --sizein --sizeout --fasta_width 0 \
     --nonchimeras "chimeras/${BASENAME}_denovo_nonchimeras.fasta" --chimeras "chimeras/${BASENAME}_denovo_chimeras.fasta" --borderline "chimeras/${BASENAME}_denovo_borderline.fasta"
 
     # Combining chimeric and borderline sequences for each file
